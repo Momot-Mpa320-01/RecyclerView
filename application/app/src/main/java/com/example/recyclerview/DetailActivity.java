e36c6abe42837de20c2313997ee629f7a3e9ad5e
Momot-Mpa320-01 <76274786+Momot-Mpa320-01@users.noreply.github.com>
2020-12-29 10:36:02 +0200

    Add recyclerview in MainActivity.java,edit DetailActivity

--- application/app/src/main/java/com/example/recyclerview/DetailActivity.java
+++ application/app/src/main/java/com/example/recyclerview/DetailActivity.java
@@ -3,12 +3,25 @@ package com.example.recyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
-
+    private TextView detail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Bundle arguments = getIntent().getExtras();
+        String id = arguments.get("ID").toString();    // Hello World
+        detail = findViewById(R.id.detail);
+        String str = "";
+        for (int i=0;i<Integer.valueOf(id);i++){
+            str+="Детальная информация";
+            str +="\n";
+        }
+        detail.setText("Информация об элементе: "+id +"\n"+str);
+
     }
 }
--- application/app/src/main/java/com/example/recyclerview/MainActivity.java
+++ application/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView ;
+    DummyContent content;
+    List<DummyContent.DummyItem> items;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.list);
+        items = content.ITEMS;
+        RViewAdapter adapter = new RViewAdapter(items);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
